45762e4ef85604dea62c9d5568882a465a1eaecf
23raven <23raven.games@gmail.com>
2026-08-31 07:11:44 +0500

    v1.05

--- experience.js
+++ experience.js
@@ -2,7 +2,14 @@ const experiences = [
     {
         company: "Prime Food Factory",
         position: "Operations Analyst",
-        description: "УУУУУУУУУУУУУrk and responsibilities. Description of work and responsibilities.",
+
+        description: [
+            "Managed and organized internal and local document workflows",
+            "Prepared official documentation for the sale of goods and services and coordinated with the accounting department",
+            "Prepared documentation to verify the proper use of loan funds for the bank and Damu Fund, and coordinated with external appraisers",
+            "Verified incoming goods against supporting documentation and coordinated workflows between warehouse, production, accounting and external organizations."
+        ],
+
         date: "Aug 2025 — Aug 2026"
     }
 ];
@@ -24,7 +31,11 @@ function renderExperiences() {
         tile.innerHTML = `
             <div class="experience-company">${experience.company}</div>
             <div class="experience-position">${experience.position}</div>
-            <div class="experience-description">${experience.description}</div>
+            <ul class="experience-description">
+    ${experience.description
+        .map((point) => `<li>${point}</li>`)
+        .join("")}
+</ul>
             <div class="experience-date">${experience.date}</div>
         `;
 
