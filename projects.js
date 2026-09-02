8c885922b29532c01d3e4360db211494d965ba44
23raven <23raven.games@gmail.com>
2026-09-02 20:35:49 +0500

    adaptive styles.css

--- projects.js
+++ projects.js
@@ -1,39 +1,160 @@
 const projects = [
     {
-        title: "Project Name",
+        title: "Zero Direction",
         technologies: "Unity · C#",
-        description: "Short description of the project, its purpose and what was implemented.",
-        logo: "images/f2.jpg",
+        description: "A simple 2D platformer built in Unity where the direction of gravity can change during gameplay",
+        logo: "images/icon_zd.png",
+
+        tags: [
+            "game-jam",
+        ],
+
+        links: {
+            itch: "https://23raven.itch.io/zero-direction",
+            youtube: "https://youtu.be/Cr8du3bMqoM",
+            github: "https://github.com/23raven/Zero-Direction"
+        }
+    },
+    {
+        title: "Tracer FPS",
+        technologies: "Unity · C#",
+        description: "Tracer FPS is a reverse-engineering project recreating Tracer’s core gameplay mechanics from Overwatch in Unity",
+        logo: "images/icon_tr.png",
 
         tags: [
             "pet-project",
-            "game-jam",
             "mechanics"
         ],
 
         links: {
-            itch: "#",
-            youtube: "#",
-            github: "#"
+            itch: "https://23raven.itch.io/tracerfps",
+            youtube: "https://youtu.be/bb6q8can3Yo",
+            github: "https://github.com/23raven/Unity_TracerFPS"
         }
     },
     {
-        title: "Solar",
+        title: "Nova Expedition",
         technologies: "Unity · C#",
-        description: "Short description of the project, its purpose and what was implemented.",
-        logo: "images/f2.jpg",
+        description: "A small story-driven adventure game developed with Unity, where the player explores a mysterious alien planet and uncovers its secrets",
+        logo: "images/icon_ne.png",
 
         tags: [
-            "pet-project",
-            "game-jam"
+            "pet-project"
         ],
 
         links: {
-            itch: "#",
-            youtube: "#",
-            github: "#"
+            itch: "https://23raven.itch.io/nova-expedition-trials",
+            youtube: "https://www.youtube.com/watch?v=IYjij2PDPBA",
+            github: "https://github.com/23raven/Unity_Nova-Expedition-Trials"
         }
-    }
+    },
+    {
+        title: "Tooth Fairy",
+        technologies: "Unity · C#",
+        description: "A small 2D game made in Unity where you play as a Tooth Fairy",
+        logo: "images/icon_tf.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            itch: "https://23raven.itch.io/tooth-fairy",
+            youtube: "https://www.youtube.com/watch?v=kRUNb_6U_4g",
+            github: "https://github.com/23raven/Unity_Tooth-Fairy"
+        }
+    },
+    {
+        title: "Solar System",
+        technologies: "Unity · C#",
+        description: "A Unity 3D project featuring an interactive Solar System with a simple orbit mini game and visual progression",
+        logo: "images/icon_ss.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=e2kvKioneVA",
+            github: "https://github.com/23raven/Unity_Solar-System"
+        }
+    },
+    {
+        title: "Rolling Ball",
+        technologies: "Unity · C#",
+        description: "Unity Rolling Ball is a simple 3D game made with Unity where the player controls a rolling ball and interacts with objects on the level",
+        logo: "images/icon_rb.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=F68Bac06U3c",
+            github: "https://github.com/23raven/Unity_Rolling-Ball"
+        }
+    },
+    {
+        title: "Sprite Flight",
+        technologies: "Unity · C#",
+        description: "The player controls a small spaceship flying inside a confined area while avoiding moving obstacles",
+        logo: "images/icon_sf.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=vYucQs1Fw9o",
+            github: "https://github.com/23raven/Unity_Sprite-Flight"
+        }
+    },
+    {
+        title: "Tanks",
+        technologies: "Unity · C#",
+        description: "Small tank battle prototype made in Unity",
+        logo: "images/icon_tks.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=bU9ElsaJo18",
+            github: "https://github.com/23raven/Unity_Tanks"
+        }
+    },
+    {
+        title: "Flappy Bird Clone",
+        technologies: "Unity · C#",
+        description: "A simple Flappy Bird clone built with Unity",
+        logo: "images/icon_fb.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=uHSEDY-xDtA",
+            github: "https://github.com/23raven/Unity_Flappy-Bird"
+        }
+    },
+    {
+        title: "Tic Tac Toe",
+        technologies: "Unity · C#",
+        description: "Simple 2D Tic Tac Toe game made in Unity",
+        logo: "images/icon_ttt.png",
+
+        tags: [
+            "pet-project"
+        ],
+
+        links: {
+            youtube: "https://www.youtube.com/watch?v=ZDp5QZ-_KfI",
+            github: "https://github.com/23raven/Unity_TicTacToe"
+        }
+    },
+    
 ];
 
 
@@ -65,44 +186,50 @@ function renderProjects() {
             .join("");
 
         tile.innerHTML = `
-            <div class="project-logo">
-                <img src="${project.logo}" alt="${project.title} logo">
-            </div>
+    <div class="project-logo">
+        <img src="${project.logo}" alt="${project.title} logo">
+    </div>
 
-            <div class="project-content">
+    <div class="project-content">
 
-                <div class="project-title">
-                    ${project.title}
-                </div>
+        <div class="project-title">
+            ${project.title}
+        </div>
 
-                <div class="project-tags">
-                    ${tags}
-                </div>
+        <div class="project-tags">
+            ${tags}
+        </div>
 
-                <div class="project-technologies">
-                    ${project.technologies}
-                </div>
+        <div class="project-technologies">
+            ${project.technologies}
+        </div>
 
-                <div class="project-description">
-                    ${project.description}
-                </div>
+        <div class="project-description">
+            ${project.description}
+        </div>
 
-                <div class="project-links">
-                    <a href="${project.links.itch}" target="_blank" rel="noopener noreferrer">
-                        Itch.io
-                    </a>
+        <div class="project-links">
+            ${project.links.itch ? `
+                <a href="${project.links.itch}" target="_blank" rel="noopener noreferrer">
+                    Itch.io
+                </a>
+            ` : ""}
 
-                    <a href="${project.links.youtube}" target="_blank" rel="noopener noreferrer">
-                        YouTube
-                    </a>
+            ${project.links.youtube ? `
+                <a href="${project.links.youtube}" target="_blank" rel="noopener noreferrer">
+                    YouTube
+                </a>
+            ` : ""}
 
-                    <a href="${project.links.github}" target="_blank" rel="noopener noreferrer">
-                        Github
-                    </a>
-                </div>
+            ${project.links.github ? `
+                <a href="${project.links.github}" target="_blank" rel="noopener noreferrer">
+                    Github
+                </a>
+            ` : ""}
+        </div>
 
-            </div>
-        `;
+    </div>
+`;
 
         container.appendChild(tile);
     });
